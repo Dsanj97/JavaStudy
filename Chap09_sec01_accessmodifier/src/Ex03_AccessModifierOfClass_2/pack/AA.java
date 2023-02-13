cd5e5683d5a4d19b2578b725f9c951a0baa566bc
Dsanj97 <dsanj97@gmail.com>
2023-02-14 01:59:22 +0900

    클래스와 생성자의 접근지정자

---
+++ Chap09_sec01_accessmodifier/src/Ex03_AccessModifierOfClass_2/pack/AA.java
@@ -0,0 +1,6 @@
+package Ex03_AccessModifierOfClass_2.pack;
+
+// pubilc class
+public class AA {
+    // pubilc 기본 생성자를 컴파일러가 추가
+}
---
+++ Chap09_sec01_accessmodifier/src/Ex03_AccessModifierOfClass_2/pack/BB.java
@@ -0,0 +1,6 @@
+package Ex03_AccessModifierOfClass_2.pack;
+
+// default class
+class BB {
+    //default 생성자를 컴파일러가 추가
+}
