723f3fc3c5b230e45e2ceca17cfdba0f87f63daf
Dsanj97 <dsanj97@gmail.com>
2023-02-14 01:40:13 +0900

    클래스와 생성자의 접근지정자

---
+++ Chap09_sec01_accessmodifier/src/Ex02_AccessModifierOfClass_1/AccessModifierOfClass_1.java
@@ -0,0 +1,12 @@
+package Ex02_AccessModifierOfClass_1;
+
+public class AccessModifierOfClass_1 {
+    // 객체생성
+    A a = new A();
+    B b = new B();
+    C c = new C();
+    /* 같은 패키지에서는 pubilc 클래스와 default클래스의타입모두
+     * 객체를 선언할수 있으며, pubilc 생성자와 default 생성자 모두 
+     * 호출이 가능하다.
+     */
+}
